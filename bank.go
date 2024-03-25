a24ba5a32542b4c40ed92c6d3d69bb488b408ca2
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-03-24 22:27:45 -0300

    banking prj foundation setup

---
+++ bank.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
+
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
+
+	if err != nil {
+		return 1000, errors.New("Failed to find balance file.")
+	}
+
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+
+	if err != nil {
+		return 1000, errors.New("Failed to parse lance file.")
+	}
+	return balance, nil
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+}
+
+func main() {
+	var accountBalance, err = getBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("- - - - - - - - - ")
+		panic("Can't continue, sorry ...")
+	}
+
+	for {
+		fmt.Println("Welcome to the Go Bank")
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance ")
+		fmt.Println("2. Deposity money")
+		fmt.Println("3. Wihdraw money")
+		fmt.Println("4. Exit")
+
+		var choise int
+		fmt.Println("Your choice:")
+		fmt.Scan(&choise)
+
+		switch choise {
+		case 1:
+			fmt.Println("Your balance is", accountBalance)
+		case 2:
+			fmt.Println("Your deposit:")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+			accountBalance += depositAmount
+			fmt.Println("Balance updated. New amount:", accountBalance)
+			writeBalanceToFile(accountBalance)
+		case 3:
+			fmt.Println("Withdrawal amount:")
+			var withdrawalAmount float64
+			fmt.Scan(&withdrawalAmount)
+
+			if withdrawalAmount <= 0 {
+				fmt.Println("Invalid withdrawal. Must be greater than 0.")
+				continue
+			}
+			if withdrawalAmount > accountBalance {
+				fmt.Println("Invalid withdrawal. You can't withdraw more than you have.")
+				continue
+			}
+
+			accountBalance -= withdrawalAmount
+			fmt.Println("Balance updated. New amount:", accountBalance)
+			writeBalanceToFile(accountBalance)
+		default:
+			fmt.Println("Goodbye")
+			fmt.Println("Thanks for choosing our bank")
+			return
+		}
+	}
+}
