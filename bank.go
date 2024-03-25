16d37a45a5150ba5b5d977c6fb474b2eec844c03
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-03-24 22:34:54 -0300

    splited communication.go in another file

--- bank.go
+++ bank.go
@@ -41,12 +41,7 @@ func main() {
 	}
 
 	for {
-		fmt.Println("Welcome to the Go Bank")
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check balance ")
-		fmt.Println("2. Deposity money")
-		fmt.Println("3. Wihdraw money")
-		fmt.Println("4. Exit")
+		presentOptions()
 
 		var choise int
 		fmt.Println("Your choice:")
